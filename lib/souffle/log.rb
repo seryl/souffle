9873e683dcad14c27052402fe101e44cf16bba77
Josh Toft <joshtoft@gmail.com>
2012-07-17 16:20:14 -0700

    Adding documentation to the Souffle logger.

--- lib/souffle/log.rb
+++ lib/souffle/log.rb
@@ -10,7 +10,9 @@ module Souffle
     # Force initialization of the primary log device (@logger)
     init
 
+    # Monkeypatch Formatter to allow local show_time updates.
     class Formatter
+      # Allow enabling and disabling of time with a singleton.
       def self.show_time=(*args)
         Mixlib::Log::Formatter.show_time = *args
       end
