dac4c3fa96ec9dacc6fcb8420dd2dac218d91c8c
Josh Toft <joshtoft@gmail.com>
2012-07-17 18:07:02 -0700

    Updated node spec

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -13,8 +13,18 @@ describe "Souffle::Node" do
   end
 
   it "should be able to setup dependencies" do
+    @node.dependencies << "role[chef_server]"
+    @node.dependencies.should eql(["role[chef_server]"])
   end
 
-  it "should fail on invalid setup dependencies" do
+  it "should fail on improper dependencies" do
+  end
+
+  it "should be able to setup the run_list" do
+    @node.run_list << "example_role"
+    @node.run_list.should eql(["example_role"])
+  end
+
+  it "should fail on improper run_list" do
   end
 end
