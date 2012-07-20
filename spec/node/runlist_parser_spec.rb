6abdea8191cd049131679b68efa712fb236204f9
Josh Toft <joshtoft@gmail.com>
2012-07-19 18:25:22 -0700

    Moving node-associated tests into a node directory

--- spec/node/runlist_parser_spec.rb
+++ spec/node/runlist_parser_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Souffle::Node::RunListParser" do
   it "should be able to check whether a hash name is a valid word" do
--- spec/node/runlist_spec.rb
+++ spec/node/runlist_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Souffle::Node::RunList" do
   it "should be able to add a runlist item onto the runlist" do
