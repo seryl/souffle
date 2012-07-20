cf5e38107bec7b079d137b6ec5b4cf39b998762a
Josh Toft <joshtoft@gmail.com>
2012-07-19 17:11:39 -0700

    Adding provider spec

---
+++ spec/provider_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Souffle::Provider" do
+  before(:each) do
+    @provider = Souffle::Provider.new
+  end
+
+  after(:each) do
+    @provider = nil
+  end
+
+  it "should raise errors on non-overridden setup" do
+    lambda { @provider.setup }.should raise_error
+  end
+
+  it "should raise errors on non-overridden name" do
+    lambda { @provider.name }.should raise_error
+  end
+
+  it "should raise errors on non-overridden create_raid" do
+    lambda { @provider.create_raid }.should raise_error
+  end
+end
