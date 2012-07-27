be5e761a70c9e9d29a1e91dde4d1cfabe07ab182
Josh Toft <joshtoft@gmail.com>
2012-07-27 14:22:07 -0700

    Cleaning up Souffle::Log

--- lib/souffle/log.rb
+++ lib/souffle/log.rb
@@ -1,22 +1,19 @@
 require 'logger'
 require 'mixlib/log'
 
-module Souffle
-  # Souffle's internal logging facility.
-  # Standardized to provide a consistent log format.
-  class Log
-    extend Mixlib::Log
+# Souffle's internal logging facility.
+# Standardized to provide a consistent log format.
+class Souffle::Log
+  extend Mixlib::Log
 
-    # Force initialization of the primary log device (@logger)
-    init
+  # Force initialization of the primary log device (@logger)
+  init
 
-    # Monkeypatch Formatter to allow local show_time updates.
-    class Formatter
-      # Allow enabling and disabling of time with a singleton.
-      def self.show_time=(*args)
-        Mixlib::Log::Formatter.show_time = *args
-      end
+  # Monkeypatch Formatter to allow local show_time updates.
+  class Formatter
+    # Allow enabling and disabling of time with a singleton.
+    def self.show_time=(*args)
+      Mixlib::Log::Formatter.show_time = *args
     end
-
   end
 end
