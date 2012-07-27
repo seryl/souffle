f2f5990bf75da49e32ee8ec445860b0624f5c436
Josh Toft <joshtoft@gmail.com>
2012-07-27 15:11:14 -0700

    Cleaning up runlist spacing

--- lib/souffle/node/runlist.rb
+++ lib/souffle/node/runlist.rb
@@ -1,24 +1,22 @@
 require 'souffle/node/runlist_item'
 
-module Souffle
-  # A specialized Array that handles runlist items appropriately.
-  class Node::RunList < Array
+# A specialized Array that handles runlist items appropriately.
+class Souffle::Node::RunList < Array
 
-    # Pushes another runlist item onto the runlist array.
-    # 
-    # @param [ String ] item The runlist item as a string.
-    def <<(item)
-      item = Souffle::Node::RunListItem.new(item)
-      super(item)
-    end
-
-    # Pushes another item onto the runlist array.
-    # 
-    # @param [ String ] item The runlist item as a string.
-    def push(item)
-      item = Souffle::Node::RunListItem.new(item)
-      super(item)
-    end
-
+  # Pushes another runlist item onto the runlist array.
+  # 
+  # @param [ String ] item The runlist item as a string.
+  def <<(item)
+    item = Souffle::Node::RunListItem.new(item)
+    super(item)
   end
+
+  # Pushes another item onto the runlist array.
+  # 
+  # @param [ String ] item The runlist item as a string.
+  def push(item)
+    item = Souffle::Node::RunListItem.new(item)
+    super(item)
+  end
+
 end
