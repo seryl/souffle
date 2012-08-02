1c076d88ef08ec584b827d1aa409584f306af2c0
Josh Toft <joshtoft@gmail.com>
2012-08-01 17:57:36 -0700

    still fleshing out AWS

--- lib/souffle/provider/aws.rb
+++ lib/souffle/provider/aws.rb
@@ -1,5 +1,6 @@
 require 'souffle/provider'
 require 'right_aws'
+require 'securerandom'
 
 # The AWS souffle provider.
 class Souffle::Provider::AWS < Souffle::Provider
@@ -19,29 +20,73 @@ class Souffle::Provider::AWS < Souffle::Provider
   # The name of the given provider.
   def name; "AWS"; end
 
+  # Generates a prefixed unique tag.
+  # 
+  # @param [ String ] tag_prefix The tag prefix to use.
+  # 
+  # @return [ String ] The unique tag with prefix.
+  def generate_tag(tag_prefix="souffle")
+    "#{tag_prefix}-#{SecureRandom.hex(6)}"
+  end
+
   # Creates a system using aws as the provider.
   # 
   # @param [ Souffle::System ] system The system to instantiate.
-  def create_system(system)
+  # @param [ String ] tag_prefix The tag prefix to use for the system.
+  def create_system(system, tag_prefix="souffle")
+    system.options[:tag] = generate_tag(tag_prefix)
     if using_vpc? and vpc_setup?
     end
   end
 
-  # Takes a list of node definitions and begins the provisioning process.
+  # Takes a list of nodes and returns the list of their aws instance_ids.
   # 
-  # @param [ Array ] nodes The list of nodes to instantiate.
-  def create_nodes(nodes)
+  # @param [ Array ] nodes The list of nodes to get instance_id's from.
+  def instance_id_list(nodes)
+    node_id_list = Array.new
+    Array(nodes).each { |n| node_id_list << n.options[:aws_instance_id] }
+    node_id_list
+  end
+
+  # Takes a node definition and begins the provisioning process.
+  # 
+  # @param [ Souffle::Node ] nodes The node to instantiate.
+  # @param [ String ] tag The tag to use for the node.
+  def create_node(node, tag="")
+    options = Hash.new
+    options[:min_count] = 1
+    options[:max_count] = 1
+
+    ebs_info = create_ebs(node)
+    @ec2.launch_instances(
+      node[:options].fetch(:aws_image_id, Souffle::Config[:aws_image_id]),
+      options)
   end
 
   # Takes a list of nodes an stops the instances.
   # 
-  # @param [ Array ] nodes The list of nodes to stop.
-  def stop_node(nodes)
+  # @param [ Souffle::Node, Array ] nodes The list of nodes to stop.
+  def stop_nodes(nodes)
+    @ec2.stop_instances(instance_id_list(nodes))
+  end
+
+  # Stops all nodes in a given system.
+  # 
+  # @param [ Souffle::System ] system The system to stop.
+  def stop_system(system)
+    stop_nodes(system.nodes)
+  end
+
+  # Takes a list of nodes and kills them. (Haha)
+  # 
+  # @param [ Souffle::Node ] nodes The list of nodes to terminate.
+  def kill_nodes(nodes)
+    @ec2.terminate_instances(instance_id_list(nodes))
   end
 
   # Creates a raid array with the given requirements.
   # 
-  # @param [ Souffle::Node ] node The node create the raid for.
+  # @param [ Souffle::Node ] node The node to the raid for.
   def create_raid(node)
   end
 
@@ -49,6 +94,10 @@ class Souffle::Provider::AWS < Souffle::Provider
   # 
   # @param [ Souffle::Node ] node The node to create ebs volumes for.
   def create_ebs(node)
+
+    @ec2.create_volume(
+      node[:options].fetch(:aws_),
+      node[:options][:aws_availability_zone])
   end
 
   # Whether or not to use a vpc instance and subnet for provisioning.
