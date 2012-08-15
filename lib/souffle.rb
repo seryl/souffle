b02cf299547ac1eae5a0e7553dbb8e4de6eb3a7a
Josh Toft <joshtoft@gmail.com>
2012-08-14 18:19:24 -0700

    Separating version

--- lib/souffle.rb
+++ lib/souffle.rb
@@ -3,12 +3,6 @@ require 'yajl'
 require 'eventmachine'
 require 'em-ssh'
 
-# An orchestrator for setting up isolated chef-managed systems.
-module Souffle
-  # The current souffle version.
-  VERSION = "0.0.1"
-end
-
 require 'souffle/ssh_monkey'
 require 'souffle/log'
 require 'souffle/exceptions'
