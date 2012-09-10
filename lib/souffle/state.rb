b602f245db2d5c67b1f85aeaf82426c5a130d7a6
Josh Toft <joshtoft@gmail.com>
2012-09-10 16:14:30 -0700

    Updated state to read from a real key

--- lib/souffle/state.rb
+++ lib/souffle/state.rb
@@ -3,9 +3,14 @@ require 'souffle/redis_client'
 class Souffle::State
   class << self
 
+    # The Souffle::State prefix for Redis.
+    def prefix
+      "souffle_state_"
+    end
+
     # Returns the current system states.
     def status
-      Souffle::Redis.get('awesome')
+      Souffle::Redis.get("#{Souffle::State.prefix}status")
     end
   end
 end
