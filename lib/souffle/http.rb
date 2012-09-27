583200b6cee354d4439cacdec8e661b1b112cb2a
Josh Toft <joshtoft@gmail.com>
2012-09-26 17:48:17 -0700

    Updated http for galaxy

--- lib/souffle/http.rb
+++ lib/souffle/http.rb
@@ -69,6 +69,33 @@ class Souffle::Http < Sinatra::Base
     end.to_json
   end
 
+  # Returns the current status of souffle.
+  get '/system' do
+    access_key    = Souffle::Config[:aws_access_key]
+    access_secret = Souffle::Config[:aws_access_secret]
+    logger = Logger.new('/dev/null')
+    ec2 = RightAws::Ec2.new(
+      access_key, access_secret,
+      :region => Souffle::Config[:aws_region],
+      :logger => logger)
+
+    ec2.describe_instances(
+        :filters => { 'tag-key' => "souffle" }).inject({}) do |slist, instance|
+      slist[instance[:tags]["souffle"]] ||= Hash.new
+      slist[instance[:tags]["souffle"]][:nodes] ||= Array.new
+      slist[instance[:tags]["souffle"]][:user]  ||= instance[:tags]["user"]
+
+      instance_info = {
+        :name => instance[:tags]["Name"],
+        :ipaddress => instance[:private_ip_address],
+        :state => instance[:aws_state].downcase
+      }
+
+      slist[instance[:tags]["souffle"]][:nodes] << instance_info
+      slist
+    end.to_json
+  end
+
   # Returns the system-specific status.
   get '/system/:system' do
     access_key    = Souffle::Config[:aws_access_key]
@@ -99,7 +126,20 @@ class Souffle::Http < Sinatra::Base
 
   # Deletes a given system.
   delete '/system/:system' do
-    { :success => false,
-      :message => "Not yet implemented." }.to_json
+    access_key    = Souffle::Config[:aws_access_key]
+    access_secret = Souffle::Config[:aws_access_secret]
+    logger = Logger.new('/dev/null')
+    ec2 = RightAws::Ec2.new(
+      access_key, access_secret,
+      :region => Souffle::Config[:aws_region],
+      :logger => logger)
+
+    remove_list = ec2.describe_instances ( :filters => {
+      'tag-key' => "souffle", 'tag-value' => params[:system] }
+      ).inject([]) do |instance_list, instance|
+      instance_list << instance[:aws_instance_id]
+      instance_list
+    end
+    ec2.terminate_instances(remove_list).to_json
   end
 end
