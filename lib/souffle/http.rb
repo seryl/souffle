2b010b8157bcdf1c5ded5660f2a893cae1ac5f1b
Josh Toft <joshtoft@gmail.com>
2012-09-26 17:50:42 -0700

    Removing junk http get

--- lib/souffle/http.rb
+++ lib/souffle/http.rb
@@ -41,33 +41,6 @@ class Souffle::Http < Sinatra::Base
       { :success => false }.to_json
     end
   end
-  
-  # Returns the current status of souffle.
-  get '/system' do
-    access_key    = Souffle::Config[:aws_access_key]
-    access_secret = Souffle::Config[:aws_access_secret]
-    logger = Logger.new('/dev/null')
-    ec2 = RightAws::Ec2.new(
-      access_key, access_secret,
-      :region => Souffle::Config[:aws_region],
-      :logger => logger)
-
-    ec2.describe_instances(
-        :filters => { 'tag-key' => "souffle" }).inject({}) do |slist, instance|
-      slist[instance[:tags]["souffle"]] ||= Hash.new
-      slist[instance[:tags]["souffle"]][:nodes] ||= Array.new
-      slist[instance[:tags]["souffle"]][:user]  ||= instance[:tags]["user"]
-
-      instance_info = {
-        :name => instance[:tags]["Name"],
-        :ipaddress => instance[:private_ip_address],
-        :state => instance[:aws_state].downcase
-      }
-
-      slist[instance[:tags]["souffle"]][:nodes] << instance_info
-      slist
-    end.to_json
-  end
 
   # Returns the current status of souffle.
   get '/system' do
