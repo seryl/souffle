fdc227b3d68a2e1db34017594596b68533959b7e
Josh Toft <joshtoft@gmail.com>
2013-01-22 11:29:50 -0800

    Update lib/souffle/provider/aws.rb

--- lib/souffle/provider/aws.rb
+++ lib/souffle/provider/aws.rb
@@ -665,10 +665,7 @@ class Souffle::Provider::AWS < Souffle::Provider::Base
   # 
   # @return [ String ] The device string to mount to.
   def volume_id_to_device(volume_id)
-    if volume_id >= 4
-      volume_id += 1
-    end
-    "/dev/xvd#{(volume_id + "a".ord).chr}"
+    "/dev/xvd#{(volume_id + "j".ord).chr}"
   end
 
   # Takes the volume count in the array and converts it to a device name.
